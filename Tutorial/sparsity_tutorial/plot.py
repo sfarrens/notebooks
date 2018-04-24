1d7a74344a9ae936210a09065c9d4d038f2a9c9c
Samuel Farrens <samuel.farrens@gmail.com>
2018-04-24 16:13:31 +0200

    updated

--- Tutorial/sparsity_tutorial/plot.py
+++ Tutorial/sparsity_tutorial/plot.py
@@ -2,15 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def stem_plot(data, title=None, imag=False):
+def stem_plot(data, x_vals=None, title=None, imag=True, ylim=None, line=False,
+              f=None):
 
-    x_vals = np.arange(data.size)
+    if x_vals is None:
+        x_vals = np.arange(data.size)
+
+    if ylim is None:
+        ylim = (-1, 2.5)
 
     (markers, stemlines, baseline) = plt.stem(x_vals, np.real(data),
                                               label='Real')
     plt.setp(stemlines, linestyle="-", color="grey", linewidth=0.5)
     plt.setp(markers, marker='o', color="#19C3F5")
     plt.setp(baseline, linestyle="--", color="grey", linewidth=2)
+    if line:
+        plt.plot(x_vals, data, linestyle="-", color='#FF4F5B')
+    if f is not None:
+        xx = np.arange(0, 1, 1/1000.)
+        plt.plot(xx, np.sin(2 * np.pi * f * xx), 'g:')
 
     if imag:
 
@@ -21,10 +31,19 @@ def stem_plot(data, title=None, imag=False):
         plt.setp(baseline, linestyle="--", color="grey", linewidth=2)
         plt.legend(loc=1)
 
-    plt.xlim(0, data.size)
-    plt.ylim(-1, 2.5)
+    # plt.xlim(0, data.size)
+    plt.ylim(ylim)
     if not isinstance(title, type(None)):
         plt.title(title, fontsize=20)
+    plt.show()
+
+
+def cost_plot(data):
+
+    plt.plot(data, linestyle='-', color='#FF4F5B')
+    plt.xlabel('Iteration', fontsize=18)
+    plt.ylabel('Cost', fontsize=18)
+    plt.title('Cost Function', fontsize=20)
     plt.show()
 
 
